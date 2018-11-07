db9b70b98386274921b53de48efd7554dbe81094
Inbal Ziv <inbal@gigya-inc.com>
2018-11-07 19:03:20 +0200

    exc 4

--- src/app/components/weather-list/weather-list.component.ts
+++ src/app/components/weather-list/weather-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Iweather} from '../../interfaces/iweather';
 
 @Component({
   selector: 'app-weather-list',
   templateUrl: './weather-list.component.html',
   styleUrls: ['./weather-list.component.css']
 })
-export class WeatherListComponent implements OnInit {
+export class WeatherListComponent implements OnInit, Iweather {
+
+  weatherData: Iweather[] = [
+    {
+      description: "d",
+      cityName: "ss",
+      temperature: 20
+    }
+  ];
   constructor() { }
 
   ngOnInit() { }
